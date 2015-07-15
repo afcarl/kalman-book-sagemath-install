c3485cd2e89c8789a09ced7c5e4399af991174d8
Roger Labbe <rlabbejr@gmail.com>
2015-07-15 13:54:48 -0700

    removed cd ..
    Can just wget this file to $HOME, no need to cd..

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd ..
 git clone --depth=1 https://github.com/rlabbe/Kalman-and-Bayesian-Filters-in-Python.git 
 cd Kalman-and-Bayesian-Filters-in-Python/ 
 git clone --depth=1 https://github.com/rlabbe/filterpy.git 
